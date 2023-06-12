a3ece1ab2e5597f20528f01edaedd2059a3feb70
Nathan Hinton <nathan@hintonclan.org>
2023-06-12 10:19:52 -0600

    changed the board to bh the SKR MINI E3 V2.0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V3_0
+  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V2_0
 #endif
 
 /**
